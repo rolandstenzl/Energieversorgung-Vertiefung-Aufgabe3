0ac6cd4fd5bb44e74f6636771d5f18c4d95cf77c
yocef-Z <josef.zeininger@gmail.com>
2026-05-29 11:54:27 +0200

    Test Josef

--- PowerFactory_v2_Task4_Task5.py
+++ PowerFactory_v2_Task4_Task5.py
@@ -5,6 +5,7 @@ from scipy.optimize import minimize
 from datetime import datetime
 import csv
 import os
+
 # =========================================================
 # CONFIGURATION 
 # =========================================================
@@ -12,7 +13,7 @@ import os
 POWERFACTORY_PYTHON = r"C:\Program Files\DIgSILENT\PowerFactory 2025 SP1\Python\3.13"
 PROJECT_NAME = "Transmission System"
 OPERATING_CASE = " "
-TIMESTAMP = "24.07.2024 14:00" # Test Max
+TIMESTAMP = "24.07.2024 14:00" # Test Max Test Josef
 
 V_MIN = 0.95
 V_MAX = 1.05
